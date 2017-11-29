8c2395757d7a1d8d409771a465c7639702b13ee5
kidkmon <kid.mon@hotmail.com>
2017-11-29 15:03:16 -0400

    Fix matriz de adjacencia

--- AED2/PP2/main.cpp
+++ AED2/PP2/main.cpp
@@ -2,161 +2,156 @@
 #include <vector>
 #include <iomanip>
 
-#define NUMVERTICES 100
-
 using namespace std;
 
-typedef int vertex;
-typedef int Weight;
-
-struct Grafo {
-    //Matriz usa as linhas 1..n e colunas 1..n, e não utiliza a linha 0 e coluna 0.
-    Weight mat[NUMVERTICES+1][NUMVERTICES+1];
-    int n, m;
-};
-
-//funções do Grafo por Struct
-void initial(Grafo &g){
-    for(int i =0; i <= g.n;i++){
-        for(int j = 0; i <= g.n;i++){
-            g.mat[i][j] = 0;
-        }
-    }
-}
-
-void insertEdge (vertex u, vertex v, Weight w, Grafo &g){
-    g.mat[u][v] = w;
-    g.mat[v][u] = w;
-    g.m++;
-}
-
-void print (Grafo g){
-    int k = 3; //largura de campo
-    cout << " ";
-    for(int j = 1;j <= g.n;j++){
-        cout << setw(k) << j;
-    }
-    cout << endl;
-    for(int j = 1;j <= g.n*k+3;j++){
-        cout << "-";
-    }
-    cout << endl;
-    for(int i = 1; i <= g.n;i++){
-        cout << setw(1) << i; cout << " |";
-        for(int j = 1; j <= g.n;j++){
-            cout << setw(k) << g.mat[i][j];
-        }
-        cout << endl;
-    }
-}
-
-/*class MST{
-public:
-    Prim(G,r,w);
-};
-
-
-MST::Prim(G,r,w){
-
-} */
-
-
-
-class Item {
-public:
-    int item;
-    Item() {}
-    Item(int item) {
-        this->item = item;
-    }
-    void print();
-
-};
-
-class Vertice {
-public:
-    Item item;
-    Vertice* prox;
-
-    Vertice () {
-        prox = NULL;
-    }
-
-    Vertice (Item item) {
-        this->item = item;
-        prox = NULL;
-    }
-};
-
-class LSE {
+template<typename T>
+class No{
 private:
-    Vertice* prim, *ult;
+    T valor;
+    No<T>* prox;
 public:
-    LSE () {
-        prim = new Vertice();
-        prim->prox = NULL;
-        ult = prim;
-    }
-
-    void insere(Item);
-    Vertice* pred(Vertice*);
-    bool vazia();
-    Vertice* busca(Item);
-    void remove(Vertice*, Item&);
-    void print();
-
+    No(){prox = NULL;}
+    No(T);
+    void setValor(T);
+    void setProx(No<T>*);
+    T getValor();
+    No<T>* getProx();
 };
 
-bool LSE::vazia() {
-    return prim == ult;
+template<typename T>
+class Lista{
+private:
+    No<T>* prim;
+    No<T>* ult;
+public:
+    Lista ();
+    ~Lista(){}
+    No<T>* getPrim();
+    No<T>* busca(T);
+    void insere(T);
+    void remove(T);
+    void print();
+};
+
+class Grafo{
+private:
+    float **matriz;
+    float ordem, tamanho;
+    void inicializador();
+public:
+    Grafo(int);
+    void inserirAresta(int,int,float);
+    void print();
+};
+
+int main() {
+
+    Grafo g(5);
+
+    g.inserirAresta(1,2,2);
+    g.inserirAresta(2,3,2);
+    g.inserirAresta(3,4,3);
+    g.inserirAresta(4,5,4);
+    g.inserirAresta(5,1,5);
+
+    g.print();
+
+    return 0;
 }
 
-void LSE::insere(Item item) {
-    ult->prox = new Vertice();
-    ult = ult->prox;
-    ult->prox = NULL;
-    ult->item = item;
+//Início do Nó
+template<typename T>
+No<T>::No(T valor) {
+    this->valor = valor;
+    this->prox = NULL;
 }
 
-Vertice* LSE::pred(Vertice* r) {
-    Vertice* p = prim->prox;
-    while (p->prox != r) {
-        p = p->prox;
+template<typename T>
+void No<T>::setValor(T valor) { this->valor = valor; }
+
+template<typename T>
+void No<T>::setProx(No* prox) { this->prox = prox; }
+
+template<typename T>
+T No<T>::getValor() { return valor; }
+
+template<typename T>
+No<T>* No<T>::getProx() { return prox; }
+
+// Início da Lista
+template <typename T>
+Lista<T>::Lista(){
+    prim = new No<T>;
+    prim->setProx(NULL);
+    ult = prim;
+}
+
+template<typename T>
+No<T>* Lista<T>::getPrim(){
+    return prim;
+}
+
+template<typename T>
+No<T>* Lista<T>::busca(T elemento){
+    No<T>* p = prim->getProx();
+    while(p != NULL && p->getValor() != elemento){
+        p = p->getProx();
     }
     return p;
 }
 
-void LSE::remove(Vertice* r, Item& item){
-    if (vazia() || r == NULL || r == prim) {
-        cout << "impossível remover\n";
+template<typename T>
+void Lista<T>::insere(T elemento){
+    ult->setProx(new No<T>);
+    ult = ult->getProx();
+    ult->setProx(NULL);
+    ult->setValor(elemento);
+}
+
+template<typename T>
+void Lista<T>::print(){
+    No<T>* p = prim->getProx();
+    while(p != NULL){
+        cout << p->getValor() << " ";
+        p = p->getProx();
     }
-    else {
-        item = r->item;
-        Vertice* p = pred(r);
-        p->prox = r->prox;
-        if (p->prox == NULL) ult = p;
-        delete r;
+    cout << endl;
+}
+
+//Início do Grafo
+Grafo::Grafo(int ordem){
+    this->ordem = ordem;
+    matriz = new float*[ordem+1];
+    for(int i = 1; i <= ordem; i++){ matriz[i] = new float[ordem+1]; }
+    inicializador();
+}
+
+void Grafo::inicializador(){
+    for(int i = 1;i <= ordem; i++){
+        for(int j = 1; j <= ordem; j++){
+            matriz[i][j] = 0;
+        }
     }
 }
 
-void LSE::print() {
-    Vertice* p = prim->prox;
-    while (p != NULL) {
-        p->item.print();
-        p = p->prox;
+void Grafo::inserirAresta(int u, int v, float peso){
+    matriz[u][v] = peso;
+    matriz[v][u] = peso;
+    tamanho++;
+}
+
+void Grafo::print(){
+    int k = 3;
+    cout << " ";
+    for(int j = 1; j <= ordem; j++){ cout << setw(k) << j; }
+    cout << endl;
+    for(int j = 1; j <= ordem*k + 3; j++){ cout << "-"; }
+    cout << endl;
+    for(int i = 1; i <= ordem; i++){
+        cout << setw(1) << i; cout << " |";
+        for(int j = 1; j <= ordem; j++){
+            cout << setw(k) << matriz[i][j];
+        }
         cout << endl;
     }
-}
-
-void Item::print() {
-    cout << item << endl;
-}
-
-
-
-
-
-int main() {
-
-    return 0;
 }
